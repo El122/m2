3e8350609ade382d99e010b5e1af531bdb594475
El122 <55270317+El122@users.noreply.github.com>
2020-07-13 22:12:58 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+
+    var show = true;
+    var countbox = ".count";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false;
+        var w_top = $(window).scrollTop();
+        var e_top = $(countbox).offset().top;
+        var w_height = $(window).height();
+        var d_height = $(document).height();
+        var e_height = $(countbox).outerHeight();
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.card').css('opacity', '1');
+            $('.count').spincrement({
+                thousandSeparator: "",
+                duration: 1200
+            });
+
+            show = false;
+        }
+    });
+
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
