d656d68662cd651eb1c0306956109d64e0d62c7a
El122 <lazyelf472@gmail.com>
2021-01-09 23:05:05 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
-    var show = true;
-    var countbox = ".count";
+    let show = true;
+    let countbox = ".count";
     $(window).on("scroll load resize", function () {
         if (!show) return false;
-        var w_top = $(window).scrollTop();
-        var e_top = $(countbox).offset().top;
-        var w_height = $(window).height();
-        var d_height = $(document).height();
-        var e_height = $(countbox).outerHeight();
+        let w_top = $(window).scrollTop();
+        let e_top = $(countbox).offset().top;
+        let w_height = $(window).height();
+        let d_height = $(document).height();
+        let e_height = $(countbox).outerHeight();
         if (w_top + 800 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
             $('.card').css('opacity', '1');
             $('.count').spincrement({
@@ -22,7 +22,10 @@ $(document).ready(function () {
 
 });
 
-var slideIndex = 1;
+let slideIndex = 1;
+let timer = setInterval(() => {
+    plusSlide();
+}, 15000);
 showSlides(slideIndex);
 
 function plusSlide() {
@@ -30,7 +33,7 @@ function plusSlide() {
 }
 
 function minusSlide() {
-    showSlides(slideIndex -= 1);  
+    showSlides(slideIndex -= 1);
 }
 
 function currentSlide(n) {
@@ -38,17 +41,19 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slide");
+    let i;
+    let slides = document.getElementsByClassName("slide");
     if (n > slides.length) {
-      slideIndex = 1
+        slideIndex = 1
     }
     if (n < 1) {
         slideIndex = slides.length
     }
     for (i = 0; i < slides.length; i++) {
+        document.getElementsByClassName("switch")[i].style.opacity = "0.5";
         slides[i].style.display = "none";
     }
 
+    document.getElementsByClassName("switch")[slideIndex - 1].style.opacity = "1";
     slides[slideIndex - 1].style.display = "block";
 }
